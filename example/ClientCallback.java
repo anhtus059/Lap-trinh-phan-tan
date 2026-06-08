89cac5494d01a46292c695c303cb66c23be3bddb
anhtus059 <122137631+anhtus059@users.noreply.github.com>
2026-06-08 15:47:10 +0700

    Delete example directory

--- example/ClientCallback.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientCallback extends Remote {
-    // Server sẽ gọi hàm này của Client để hiển thị tin nhắn mới
-    void receiveMessage(String message) throws RemoteException;
-}
